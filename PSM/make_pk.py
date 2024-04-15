a668749568e7f0d947eec2944f033d9959baa28e
Conviss <shiweikang0318@163.com>
2024-04-15 19:31:29 +0800

    commit

---
+++ PSM/make_pk.py
@@ -0,0 +1,16 @@
+import os
+import pickle as pk
+
+import numpy as np
+import pandas as pd
+
+pth = './'
+
+train_data = pd.read_csv('train.csv').to_numpy()
+test_data = pd.read_csv('test.csv').to_numpy()
+test_label = pd.read_csv('test_label.csv').to_numpy()
+
+with open(pth + 'PSM.pk', 'wb') as file:
+    pk.dump({'train_data': train_data[:, 1:], 'test_data': test_data[:, 1:], 'test_label': test_label[:, 1]}, file)
+
+print('done')
---
+++ SMD/make_pk.py
@@ -0,0 +1,29 @@
+import os
+import pickle as pk
+
+import numpy as np
+import pandas as pd
+
+pth = './'
+# 定义自定义的排序键函数
+def natural_sort_key(s):
+    import re
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
+
+# 对文件名列表进行自然排序
+ent_names = sorted(os.listdir(pth + 'train'), key=natural_sort_key)
+
+train_data, test_data, test_label = [], [], []
+for ent_name in ent_names:
+    train_data.append(pd.read_csv(pth + 'train/' + ent_name, header=None).to_numpy())
+    test_data.append(pd.read_csv(pth + 'test/' + ent_name, header=None).to_numpy())
+    test_label.append(np.squeeze(pd.read_csv(pth + 'test_label/' + ent_name, header=None).to_numpy()))
+
+train_data = np.concatenate(train_data, axis=0)
+test_data = np.concatenate(test_data, axis=0)
+test_label = np.concatenate(test_label, axis=0)
+
+with open(pth + 'SMD.pk', 'wb') as file:
+    pk.dump({'train_data': train_data, 'test_data': test_data, 'test_label': test_label}, file)
+
+print('done')
